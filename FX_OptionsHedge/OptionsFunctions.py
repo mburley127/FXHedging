cb86c3fa1b4bb0378688bb88fc8134bda8c577b7
Matthew <matthew.burley@wsu.edu>
2024-07-01 21:02:02 -0400

    Draft Completed, further testing required

--- FX_OptionsHedge/OptionsFunctions.py
+++ FX_OptionsHedge/OptionsFunctions.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import warnings
+from scipy.stats import norm
 warnings.filterwarnings("ignore")
 
 
@@ -22,6 +23,21 @@ def import_fx_data(tickers, start_date):
     data['Date'] = pd.to_datetime(data['Date'])
 
     return data
+
+
+### Function to Get Most Recent Stock Price (S_0) and Set Strike (K)
+def set_price_and_strike(pair, start_date, option_type):
+    # Function Call to get FX Data
+    forex_data = import_fx_data(pair, start_date) 
+    # Get the Spot Price (Most recent FX rate value)
+    S_0 = forex_data[pair].iloc[-1]  # Extract the last value from the pair column (ex. 'USDEUR=X')
+    # Set Strike Price 2% above (call) or 2% below (put) S_0
+    if option_type == 'call':
+        K = 1.02*S_0
+    elif option_type == 'put':
+        K = 0.98*S_0
+
+    return forex_data, S_0, K
 
 
 ### Plot FX Rate Data
@@ -54,3 +70,101 @@ def daily_returns(fx_data):
     rets.reset_index(inplace = True)
 
     return rets
+
+
+### Function to compute sigma (volatility)
+def compute_sigma(returns):
+    if 'Date' in returns.columns:
+        returns = returns.drop(columns = 'Date')
+    # Sigma computation with removed date col
+    sigma = np.std(returns) * np.sqrt(252)  # Annualize the volatility
+    return sigma.iloc[-1]
+
+
+## Function to Build Black Scholes (BSM) Model
+def black_scholes(S_0, K, r, r_f, T, sigma, option_type):
+    # Compute d_1 and d_2
+    d_1 = (np.log(S_0 / K) + (r - r_f + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    #print(d_1)
+    d_2 = d_1 - sigma * np.sqrt(T)
+    #print(d_2)
+    
+    # Compute Option Premium and Greeks
+    if option_type == 'call':
+        option_price = S_0 * np.exp(-r_f * T) * norm.cdf(d_1) - K * np.exp(-r * T) * norm.cdf(d_2)
+        delta = np.exp(-r_f * T) * norm.cdf(d_1)
+        theta = (-S_0 * sigma * np.exp(-r_f * T) * norm.pdf(d_1) / (2 * np.sqrt(T)) 
+                 - r * K * np.exp(-r * T) * norm.cdf(d_2)
+                 + r_f * S_0 * np.exp(-r_f * T) * norm.cdf(d_1))
+        rho = K * T * np.exp(-r * T) * norm.cdf(d_2)
+
+    elif option_type == 'put':
+        option_price = K * np.exp(-r * T) * norm.cdf(-d_2) - S_0 * np.exp(-r_f * T) * norm.cdf(-d_1)
+        delta = -np.exp(-r_f * T) * norm.cdf(-d_1)
+        theta = (-S_0 * sigma * np.exp(-r_f * T) * norm.pdf(d_1) / (2 * np.sqrt(T)) 
+                 - r * K * np.exp(-r * T) * norm.cdf(-d_2)
+                 + r_f * S_0 * np.exp(-r_f * T) * norm.cdf(-d_1))
+        rho = -K * T * np.exp(-r * T) * norm.cdf(-d_2)
+
+    else:
+        raise ValueError('Error: Incorrect Option Type, must input call or put')
+
+    # Calculate Remaining Greeks
+    gamma = np.exp(-r_f * T) * norm.pdf(d_1) / (S_0 * sigma * np.sqrt(T))
+    vega = S_0 * np.exp(-r_f * T) * np.sqrt(T) * norm.pdf(d_1)
+
+    # Probabilities
+    prob_ITM_call = norm.cdf(d_1)
+    prob_ITM_put = norm.cdf(-d_1)
+    prob_exercise_call = norm.cdf(d_2)
+    prob_exercise_put = norm.cdf(-d_2)
+
+    # Return Option Premium and Greeks
+    return option_price, delta, gamma, theta, rho, vega, prob_ITM_call, prob_ITM_put, prob_exercise_call, prob_exercise_put
+
+
+### Function to Implement the Hedge (Compute Hedge Position and hedged returns)
+def hedge_pos_and_rets(fx_rets, delta, long_pos):
+    if 'Date' in fx_rets.columns:
+        fx_rets = fx_rets.drop(columns='Date')
+    
+    hedge_pos = -delta * long_pos
+    hedged_rets = fx_rets + hedge_pos
+    return hedge_pos, hedged_rets
+
+
+### Calculate Hedged/Unhedged Returns
+def calculate_hedged_unhedged(pair, long_pos, returns, option_delta):
+    # Remove Date Col as needed
+    if 'Date' in returns.columns:
+        returns = returns.set_index('Date')
+    
+    # Select the returns for the specified pair
+    pair_returns = returns[pair]
+
+    # Compute daily returns for the long position
+    long_rets = pair_returns * long_pos
+
+    # Compute Hedge Position: standard option hedge
+    hedge_pos = option_delta * long_pos
+
+    # Compute the returns of the hedged position
+    hedged_rets = long_rets - hedge_pos * pair_returns
+
+    # Compute cumulative returns for the hedged and unhedged positions
+    hedged_cumulative_rets = (1 + hedged_rets / long_pos).cumprod() * long_pos
+    unhedged_cumulative_rets = (1 + pair_returns).cumprod() * long_pos
+
+    return hedged_cumulative_rets, unhedged_cumulative_rets
+
+
+def plot_hedged_returns(hedged_cumulative_rets, unhedged_cumulative_rets):
+    plt.figure(figsize = (8, 5))
+    plt.plot(hedged_cumulative_rets.index, hedged_cumulative_rets, label = 'Hedged Returns')
+    plt.plot(unhedged_cumulative_rets.index, unhedged_cumulative_rets, label = 'Unhedged Returns')
+    plt.xlabel('Periods')
+    plt.ylabel('Cumulative Returns')
+    plt.title('Hedged vs. Unhedged Cumulative Returns for FX Option Hedging Strategy')
+    plt.legend()
+
+    return plt.show()
